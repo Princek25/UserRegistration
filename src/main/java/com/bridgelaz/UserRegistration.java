660fac577aa5b94e33f465993ed815005fb8c160
Prince <princek2530@gmail.com>
2022-08-29 21:58:17 +0530

    Added: Email Validation

--- src/main/java/com/bridgelaz/UserRegistration.java
+++ src/main/java/com/bridgelaz/UserRegistration.java
@@ -6,12 +6,15 @@ import java.util.regex.Pattern;
 public class UserRegistration {
     static Scanner sc =  new Scanner(System.in);
     static Pattern string = Pattern.compile("^[A-Z][a-z]{3,}$");
+    static Pattern email = Pattern.compile("^[a-z+.]+@(.+)$");
 
     public static void main(String[] args) {
         System.out.println("Enter your first name:- ");
         String firstName = sc.next();
         System.out.println("Enter your last name:- ");
         String lastName = sc.next();
+        System.out.println("Enter a valid email \n eg:- abc.xyz@bl.co.in");
+        String emailValid = sc.next();
 
         Matcher matchFirstName = string.matcher(firstName);
         Matcher matchLastName = string.matcher(lastName);
@@ -20,6 +23,13 @@ public class UserRegistration {
         }
         else{
             System.out.println("Invalid Name ! \n First letter should be capital only and other should be in small");
+        }
+        Matcher matchEmail = email.matcher(emailValid);
+        if(matchEmail.matches()){
+            System.out.println("Valid Email");
+        }
+        else{
+            System.out.println("Invalid Email");
         }
     }
 }
